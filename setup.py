b339d789cfe37ff53be6bb5fb72cfef507804479
Кирилл Румянцев <KARum2004@yandex.ru>
2024-06-05 17:09:36 +0300

    try to release

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
    name='pypackage',
    description='good job',
+   long_description_content_type="text/markdown",
    author='kirill',
    author_email='KARum2004@yandex.ru',
    packages=['package1'],
