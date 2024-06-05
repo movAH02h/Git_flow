5b758aa357ad8deb220cb09f5f517418bbc6a93a
Кирилл Румянцев <KARum2004@yandex.ru>
2024-06-05 17:12:18 +0300

    try to release

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
 
+long_description = open('README.md').read()
+
 setup(
    name='pypackage',
    description='good job',
    long_description_content_type="text/markdown",
+   long_description=long_description,
    author='kirill',
    author_email='KARum2004@yandex.ru',
    packages=['package1'],
